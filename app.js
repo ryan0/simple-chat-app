3f7629a902a427631dc2a2b329d818b2b760d4a4
Ryan Miller <ryan@rmiller.dev>
2022-03-27 03:26:30 -0400

    add port to env file

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/login', (req, res) => {
 httpServer.listen(process.env.PORT, () => {
     console.log('listening on 3000');
 });
-httpsServer.listen(443, () => {
+httpsServer.listen(HTTPS_PORT, () => {
     console.log('listening on 80');
 });
 
